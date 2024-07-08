934a2d8033cf919cafa3dbd68ad5a52addc7d042
hanglma <martin.hangl@jhg-traunreut.de>
2024-07-08 11:14:41 +0200

    add: window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setName("2d Game");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
