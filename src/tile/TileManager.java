ca37e18f547606f943071f2ad13de52bd783c799
ProfChristmas <noeljivan@icloud.com>
2024-07-09 11:19:16 +0200

    Texturen, Spielfeld Tic Tac Toe

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -17,7 +17,7 @@ public class TileManager {
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        tile = new Tile[10];
+        tile = new Tile[100];
         mapTileNum = new int[gp.maxScreenCol][gp.getMaxScreenRow];
 
         getTileImage();
@@ -32,8 +32,38 @@ public class TileManager {
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("floor01.png"));
 
+            //tile[2] = new Tile();
+            //tile[2].image = ImageIO.read(getClass().getResourceAsStream("grass00.png"));
+
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("grass00.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("Leer.png"));
+
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("LO.png"));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("MO.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("RO.png"));
+
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("LM.png"));
+
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream("MM.png"));
+
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream("RM.png"));
+
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream("LU.png"));
+
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream("MU.png"));
+
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(getClass().getResourceAsStream("RU.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
