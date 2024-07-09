840074a1acb9aa405ea6be9714f9e3941bf6b854
ProfChristmas <noeljivan@icloud.com>
2024-07-09 16:29:18 +0200

    nix

---
+++ src/MenschAergereDichNicht/Spielfeld.java
@@ -0,0 +1,40 @@
+package MenschAergereDichNicht;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Spielfeld
+{
+    public Map<Integer, Integer> spielfeldAll;
+    public int[][] SpielfeldFig;
+
+    public boolean feststellen;
+
+    public int x;
+    public int i, e;
+
+    public Spielfeld()
+    {
+        for(i = 0; i < 40; i++)
+        {
+            if(e == positionFeld)
+            {
+            SpielfeldAll.put(i, e);
+            }
+        }
+    }
+
+    /*public Spielfeld()
+    {
+        int e, n, m;
+
+        for(e = 0; e < SpielfeldAll.length; e++)
+        {
+
+        }
+
+    }*/
+
+
+
+}
