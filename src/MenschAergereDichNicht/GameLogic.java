7659c8beefbd46f00730789c66cbfc4f7f2dcd27
Franziska-Sch <franziska.schneider2010@gmail.com>
2024-07-11 13:07:18 +0200

    add: Menschärger dich nicht: GameLogic

--- src/MenschAergereDichNicht/GameLogic.java
+++ src/MenschAergereDichNicht/GameLogic.java
@@ -18,22 +18,14 @@ public class GameLogic
     private int anzahlBlau;
     private int rotpos, gelbpos, gruenpos, blaupos, i;
     private int mitzaehlen = 0;
+    public Map<Integer, Integer> spielfeldAll = new HashMap<Integer, Integer>();
 
-
-    public class Spielfeld
+    public void Spielfeldeintragen(int Feldall, String farb)
     {
-        public Map<Integer, Integer> spielfeldAll;
 
-        public boolean feststellen;
+        int Farbnum;
+        int i, l, pos, farbp;
 
-        public int Feldall, Farbnum;
-        public int i, l, pos, farbp;
-        public String farb;
-
-        public Spielfeld(int Feldall, String farb)
-        {
-            this.farb = farb;
-            this.Feldall = Feldall;
 
             if(farb == "rot")
             {
@@ -55,8 +47,35 @@ public class GameLogic
                 Farbnum = 0;
             }
             spielfeldAll.put(Feldall, Farbnum);
-        }
 
+    }
+
+    public String Farbauslesung(int Feld)
+    {
+        String farbe;
+        int Feldnummer;
+        Feldnummer = spielfeldAll.get(Feld);
+
+        if(Feldnummer == 1)
+        {
+            farbe = "rot";
+        }
+        if(Feldnummer == 2)
+        {
+            farbe = "blau";
+        }
+        if(Feldnummer == 3)
+        {
+            farbe = "gelb";
+        }
+        if(Feldnummer == 4)
+        {
+            farbe = "gruen";
+        }else
+        {
+            farbe = "leer";
+        }
+        return farbe;
     }
 
 
@@ -134,27 +153,91 @@ public class GameLogic
            }
 
 
-       for(i = 0; i < 4; i++)
-       {
-           if (i == 0)
-           {
-               int Wuerfelzahl = wuerfel.wuerfeln();
-               if (Wuerfelzahl == 6)
-                {
-                    figurenRot[anzahlRot]=new SpielfigurRot();
-                    /*ausgewählte Figur*/figurenRot[anzahlRot].ziehen(Wuerfelzahl);             //noch falsch
-                    rotpos = figurenRot[anzahlRot].getPositionFeld();
-                }
-               else
-                {
-                    /*ausgewählte Figur*/.ziehen(Wuerfelzahl);
-                }
-               spielfeldall = spielfeldall(rotpos, "rot");
 
-           }
-
-       }
 
     }
+
+    public void Zug(String Farbe, int Anzahl, int Wuerfelzahl){
+
+        //int Wuerfelzahl = wuerfel.wuerfeln();
+
+        switch (Farbe)
+        {
+
+            case "rot":
+
+
+                if (Wuerfelzahl == 6)
+                    {
+                        Wuerfelzahl = wuerfel.wuerfeln() + Wuerfelzahl;
+                        /*ausgewählte Figur*/figurenRot[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                        rotpos = figurenRot[Anzahl].getPositionFeld();
+                    }
+                else
+                    {
+                        /*ausgewählte Figur*/figurenRot[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                        rotpos = figurenRot[Anzahl].getPositionFeld();
+                    }
+
+                Spielfeldeintragen(rotpos,"rot");
+                break;
+
+            case "blau":
+
+                if (Wuerfelzahl == 6)
+                {
+                    Wuerfelzahl = wuerfel.wuerfeln() + Wuerfelzahl;
+                    /*ausgewählte Figur*/figurenBlau[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    blaupos = figurenBlau[Anzahl].getPositionFeld();
+                }
+                else
+                {
+                    /*ausgewählte Figur*/figurenBlau[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    blaupos = figurenBlau[Anzahl].getPositionFeld();
+                }
+
+                Spielfeldeintragen(blaupos,"blau");
+                break;
+
+            case "gelb":
+
+                if (Wuerfelzahl == 6)
+                {
+                    Wuerfelzahl = wuerfel.wuerfeln() + Wuerfelzahl;
+                    /*ausgewählte Figur*/figurenGelb[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    gelbpos = figurenGelb[Anzahl].getPositionFeld();
+                }
+                else
+                {
+                    /*ausgewählte Figur*/figurenGelb[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    gelbpos = figurenGelb[Anzahl].getPositionFeld();
+                }
+
+                Spielfeldeintragen(gelbpos,"gelb");
+                break;
+
+            case "gruen":
+
+                if (Wuerfelzahl == 6)
+                {
+                    Wuerfelzahl = wuerfel.wuerfeln() + Wuerfelzahl;
+                    /*ausgewählte Figur*/figurenGruen[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    gruenpos = figurenGruen[Anzahl].getPositionFeld();
+                }
+                else
+                {
+                    /*ausgewählte Figur*/figurenGruen[Anzahl].ziehen(Wuerfelzahl);     //noch falsch
+                    gruenpos = figurenGruen[Anzahl].getPositionFeld();
+                }
+
+                Spielfeldeintragen(gruenpos,"gruen");
+                break;
+
+            }
+
+        }
+
+    }
+
 
 }
